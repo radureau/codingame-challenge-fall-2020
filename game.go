431ac66773da8095b469aacd18b109a914d87825
Gabriel Radureau <gradureau@figarocms.fr>
2020-11-12 23:06:24 +0100

    start wood 1 league

--- game.go
+++ game.go
@@ -6,7 +6,8 @@ import (
 )
 
 const (
-	MAX_ORDERS = 5
+	MAX_ORDERS      = 5
+	MAX_INGREDIENTS = 10
 )
 
 type Point int
@@ -30,6 +31,19 @@ func (igts Ingredients) Add(i0, i1, i2, i3 int) Ingredients {
 		igts[3]+i3,
 	)
 }
+func (igts Ingredients) AddIngredients(other Ingredients) Ingredients {
+	return igts.Add(other[0], other[1], other[2], other[3])
+}
+func (igts Ingredients) IsLegit() bool {
+	sum := 0
+	for _, q := range igts {
+		if q < 0 {
+			return false
+		}
+		sum += q
+	}
+	return sum <= MAX_INGREDIENTS
+}
 
 type ActionType string
 
@@ -38,6 +52,7 @@ const (
 	CAST          = ActionType("CAST")
 	OPPONENT_CAST = ActionType("OPPONENT_CAST")
 	LEARN         = ActionType("LEARN")
+	REST          = ActionType("REST")
 )
 
 type Action struct {
@@ -49,6 +64,32 @@ type Action struct {
 	Repeatable bool
 	TaxCount   int // the amount of taxed tier-0 ingredients you gain from learning this spell
 	TomeIndex  int // the index in the tome if this is a tome spell, equal to the above tax
+}
+
+func (a Action) String() string {
+	switch a.Type {
+	case BREW, CAST:
+		return fmt.Sprintf("%s %d", a.Type, a.ID)
+	case REST:
+		return string(REST)
+	default:
+		return string(a.Type)
+	}
+}
+
+func (a Action) IsPossibleFor(witch Witch) bool {
+	switch a.Type {
+	case BREW:
+		return witch.AddIngredients(a.Ingredients).IsLegit()
+	case CAST:
+		return witch.Me && a.Castable && witch.AddIngredients(a.Ingredients).IsLegit()
+	case OPPONENT_CAST:
+		return !witch.Me && a.Castable && witch.AddIngredients(a.Ingredients).IsLegit()
+	case REST:
+		return true
+	default:
+		return false
+	}
 }
 
 func (a Action) IsLessThan(other Action) bool {
@@ -70,25 +111,25 @@ func (as ActionSlice) Pick() Action {
 func main() {
 
 	for {
-		Actions := ActionSlice{}
+		Actions := ActionSlice{{Type: REST}}
 
 		var actionCount int // actionCount: the number of spells and recipes in play
 		fmt.Scan(&actionCount)
 		for i := 0; i < actionCount; i++ {
 			var delta0, delta1, delta2, delta3, _castable, _repeatable int
 			action := Action{}
-			fmt.Scan(&action.ID, &action.Type, &delta0, &delta1, &delta2, &delta3, &action.Points, action.TomeIndex, action.TaxCount, &_castable, &_repeatable)
+			fmt.Scan(&action.ID, &action.Type, &delta0, &delta1, &delta2, &delta3, &action.Points, &action.TomeIndex, &action.TaxCount, &_castable, &_repeatable)
 			action.Castable, action.Repeatable = _castable != 0, _repeatable != 0
 			action.Ingredients = NewIngredients(delta0, delta1, delta2, delta3)
 
-			switch ActionType(action.Type) {
-			case BREW:
-			case CAST, OPPONENT_CAST, LEARN:
+			switch action.Type {
+			case BREW, CAST, OPPONENT_CAST:
+				Actions = append(Actions, action)
+			case LEARN:
 				fallthrough
 			default:
-				panic(fmt.Errorf("unknown action type %s", action.Type))
+				panic(fmt.Errorf("unknown action type %s\n%+v", action.Type, action))
 			}
-			Actions = append(Actions, action)
 		}
 
 		var inv0, inv1, inv2, inv3 int
@@ -101,7 +142,13 @@ func main() {
 		fmt.Scan(&inv0, &inv1, &inv2, &inv3, &OPNT.Points)
 		OPNT.Ingredients = NewIngredients(inv0, inv1, inv2, inv3)
 
+		NextTurnPossibilities := ActionSlice{}
+		for _, a := range Actions {
+			if a.IsPossibleFor(ME) {
+				NextTurnPossibilities = append(NextTurnPossibilities, a)
+			}
+		}
 		// in the first league: BREW <id> | WAIT; later: BREW <id> | CAST <id> [<times>] | LEARN <id> | REST | WAIT
-		fmt.Printf("BREW %d\n", Actions.Pick().ID)
+		fmt.Println(NextTurnPossibilities.Pick())
 	}
 }
